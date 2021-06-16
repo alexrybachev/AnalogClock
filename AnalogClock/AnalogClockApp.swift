4a8988cd106155d9d8949a8bd7ed69869e9b2426
Alex Rybachev <rybachev.as@gmail.com>
2021-06-16 09:44:49 +0400

    Initial Commit

---
+++ AnalogClock/AnalogClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalogClockApp.swift
+//  AnalogClock
+//
+//  Created by Aleksandr Rybachev on 16.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnalogClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
